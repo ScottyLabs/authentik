d0042f486410fbd53899fc37f2189401064bfbb6
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-09-14 00:28:46 -0400

    feat: turn hashicorp file into a class with better logging

--- scripts/hashicorp.py
+++ scripts/hashicorp.py
@@ -4,16 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-token = os.getenv("HASHICORP_API_TOKEN")
-api_url = os.getenv("HASHICORP_API_URL")
 
-ADMIN_SUFFIX = "admins"
-DEVELOPER_SUFFIX = "devs"
+class HashicorpManager:
+    ADMIN_SUFFIX = "admins"
+    DEVELOPER_SUFFIX = "devs"
 
-
-def create_admin_policy(project_name):
-    print(f"Creating admin policy for {project_name}")
-    policy = """\
+    def __init__(self, project_name):
+        self.project_name = project_name
+        self.token = os.getenv("HASHICORP_API_TOKEN")
+        self.api_url = os.getenv("HASHICORP_API_URL")
+        self.admin_policy = """\
 path "/ScottyLabs/data/{project_name}/*" {{
     capabilities = ["create", "read", "update", "delete", "list", "sudo"]
 }}
@@ -23,87 +23,97 @@ path "/ScottyLabs/metadata/{project_name}/*" {{
 }}\
 """.format(project_name=project_name)
 
-    response = requests.post(
-        f"{api_url}/v1/sys/policy/{project_name}-{ADMIN_SUFFIX}",
-        json={"policy": policy},
-        headers={"X-Vault-Token": token},
-    )
-    print(response)
-
-
-def create_developer_policy(project_name):
-    print(f"Creating developer policy for {project_name}")
-    policy = """\
+        self.developer_policy = """\
 path "/ScottyLabs/data/{project_name}/*" {{
     capabilities = ["read", "list"]
-}}\
+}}
 """.format(project_name=project_name)
-    response = requests.post(
-        f"{api_url}/v1/sys/policy/{project_name}-{DEVELOPER_SUFFIX}",
-        json={"policy": policy},
-        headers={"X-Vault-Token": token},
-    )
-    try:
-        print(response.json())
-    except Exception:
+
+    def _print_response(self, response):
         print(response)
+        try:
+            print(response.json())
+        except Exception:
+            pass
+        print()
 
+    def create_admin_policy(self, project_name):
+        print(f"Creating admin policy for {project_name}")
+        response = requests.post(
+            f"{self.api_url}/v1/sys/policy/{project_name}-{self.ADMIN_SUFFIX}",
+            json={"policy": self.admin_policy},
+            headers={"X-Vault-Token": self.token},
+        )
+        self._print_response(response)
 
-def get_group_id(project_name, suffix):
-    # First try to get the group ID if the group already exists
-    response = requests.get(
-        f"{api_url}/v1/identity/group/name/{project_name}-{suffix}",
-        headers={"X-Vault-Token": token},
-    )
-    if response.ok:
+    def create_developer_policy(self, project_name):
+        print(f"Creating developer policy for {project_name}")
+        response = requests.post(
+            f"{self.api_url}/v1/sys/policy/{project_name}-{self.DEVELOPER_SUFFIX}",
+            json={"policy": self.developer_policy},
+            headers={"X-Vault-Token": self.token},
+        )
+        self._print_response(response)
+
+    def get_group_id(self, project_name, suffix):
+        # First try to get the group ID if the group already exists
+        response = requests.get(
+            f"{self.api_url}/v1/identity/group/name/{project_name}-{suffix}",
+            headers={"X-Vault-Token": self.token},
+        )
+        if response.ok:
+            return response.json()["data"]["id"]
+
+        # If the group doesn't exist, create it
+        print(f"Creating group for {project_name}-{suffix}")
+        response = requests.post(
+            f"{self.api_url}/v1/identity/group",
+            json={
+                "name": f"{project_name}-{suffix}",
+                "type": "external",
+                "policies": [f"{project_name}-{suffix}"],
+            },
+            headers={"X-Vault-Token": self.token},
+        )
         return response.json()["data"]["id"]
 
-    # If the group doesn't exist, create it
-    print(f"Creating group for {project_name}-{suffix}")
-    response = requests.post(
-        f"{api_url}/v1/identity/group",
-        json={
-            "name": f"{project_name}-{suffix}",
-            "type": "external",
-            "policies": [f"{project_name}-{suffix}"],
-        },
-        headers={"X-Vault-Token": token},
-    )
-    return response.json()["data"]["id"]
+    def get_oidc_mount_accessor(self):
+        response = requests.get(
+            f"{self.api_url}/v1/sys/auth",
+            headers={"X-Vault-Token": self.token},
+        )
+        return response.json()["data"]["oidc/"]["accessor"]
 
-
-def get_oidc_mount_accessor():
-    response = requests.get(
-        f"{api_url}/v1/sys/auth",
-        headers={"X-Vault-Token": token},
-    )
-    return response.json()["data"]["oidc/"]["accessor"]
-
-
-def create_group(project_name, suffix, group_id):
-    print(f"Group ID: {group_id}")
-    print(f"Creating group alias for {project_name}-{suffix}")
-    response = requests.post(
-        f"{api_url}/v1/identity/group-alias",
-        json={
-            "name": f"{project_name}-{suffix}",
-            "mount_accessor": get_oidc_mount_accessor(),
-            "canonical_id": group_id,
-        },
-        headers={"X-Vault-Token": token},
-    )
-    try:
-        print(response.json())
-    except Exception:
-        print(response)
+    def create_group(self, project_name, suffix, group_id):
+        print(f"Group ID: {group_id}")
+        print(f"Creating group alias for {project_name}-{suffix}")
+        response = requests.post(
+            f"{self.api_url}/v1/identity/group-alias",
+            json={
+                "name": f"{project_name}-{suffix}",
+                "mount_accessor": self.get_oidc_mount_accessor(),
+                "canonical_id": group_id,
+            },
+            headers={"X-Vault-Token": self.token},
+        )
+        self._print_response(response)
 
 
 if __name__ == "__main__":
     project_name = input("Enter the project name: ").strip().lower()
-    create_admin_policy(project_name)
-    group_id = get_group_id(project_name, ADMIN_SUFFIX)
-    create_group(project_name, ADMIN_SUFFIX, group_id)
+    hashicorp_manager = HashicorpManager(project_name)
+    hashicorp_manager.create_admin_policy(project_name)
+    group_id = hashicorp_manager.get_group_id(
+        project_name, hashicorp_manager.ADMIN_SUFFIX
+    )
+    hashicorp_manager.create_group(
+        project_name, hashicorp_manager.ADMIN_SUFFIX, group_id
+    )
 
-    create_developer_policy(project_name)
-    group_id = get_group_id(project_name, DEVELOPER_SUFFIX)
-    create_group(project_name, DEVELOPER_SUFFIX, group_id)
+    hashicorp_manager.create_developer_policy(project_name)
+    group_id = hashicorp_manager.get_group_id(
+        project_name, hashicorp_manager.DEVELOPER_SUFFIX
+    )
+    hashicorp_manager.create_group(
+        project_name, hashicorp_manager.DEVELOPER_SUFFIX, group_id
+    )
